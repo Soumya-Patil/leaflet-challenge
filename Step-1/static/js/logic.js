2cab2d9942d639bb0287f64b2d28e5683ae516b3
Soumya Patil <getme1104@gmail.com>
2020-06-24 20:23:33 -0700

    created a geoJSON layer on the EarthQuake Data Object

--- Step-1/static/js/logic.js
+++ Step-1/static/js/logic.js
@@ -46,4 +46,20 @@ function createFeatures(earthquakeData) {
 }
 
 
+// Create a GeoJSON layer containing the features array on the earthquakeData object
+// Run the onEachFeature function once for each piece of data in the array
+var earthquakes = L.geoJSON(earthquakeData, {
+    pointToLayer: function(earthquakeData, latlng) {
+    return L.circle(latlng, {
+        radius: radiusSize(earthquakeData.properties.mag),
+        color: circleColor(earthquakeData.properties.mag),
+        fillOpacity: 1
+    });
+    },
+    onEachFeature: onEachFeature
+});
+
+// Sending our earthquakes layer to the createMap function
+createMap(earthquakes);
+
 }
